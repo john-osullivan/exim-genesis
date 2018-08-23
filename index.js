ff266a78853d64ab77c76a112d5cfab04f23e67c
John O'Sullivan <john@eximchain.com>
2018-08-23 15:43:20 -0400

    Upgraded genesis-generator to support new governance contract & governanceOwners, loaded a sampleConfig w/ addresses from Ganache

--- index.js
+++ index.js
@@ -4,13 +4,15 @@
 
 const fs = require('fs');
 const path = require('path');
+var uniq = require('lodash.uniq');
 const utils = require('ethereumjs-util');
 let template = require('./template.json');
 
-const CONFIG_FILENAME = 'quorum-config.json';
+const CONFIG_FILENAME = 'sampleConfig.json';
 const OUTPUT = 'quorum-genesis.json';
 
 const VOTING_CONTRACT_ADDR = '0x0000000000000000000000000000000000000020';
+const GOVERNANCE_CONTRACT_ADDR = '0x0000000000000000000000000000000000000042';
 
 function padIndex(number, prefix) {
   if(prefix) {
@@ -30,28 +32,36 @@ function storageKey(index, address) {
   return utils.addHexPrefix(result)
 }
 
-function mapAddresses(index, addresses) {
+function mapAddresses(index, addresses, storageAddress) {
   let value = utils.intToHex(1);
   for(let i=0; i<addresses.length; i++) {
     let key = storageKey(index, addresses[i]);
-    template['alloc'][VOTING_CONTRACT_ADDR].storage[key] = value;
+    template['alloc'][storageAddress].storage[key] = value;
   }
 }
 
-function buildStorage(input) {
+function buildVotingStorage(input) {
   template['alloc'][VOTING_CONTRACT_ADDR].storage[padIndex(1,true)] = utils.intToHex(input.threshold);
-  template['alloc'][VOTING_CONTRACT_ADDR].storage[padIndex(2,true)] = utils.intToHex(input.voters.length);
-  mapAddresses(3, input.voters);
-  template['alloc'][VOTING_CONTRACT_ADDR].storage[padIndex(4,true)] = utils.intToHex(input.makers.length);
-  mapAddresses(5,input.makers);
+  template['alloc'][VOTING_CONTRACT_ADDR].storage[padIndex(2,true)] = utils.intToHex(input.blockVoters.length);
+  mapAddresses(3, input.blockVoters, VOTING_CONTRACT_ADDR);
+  template['alloc'][VOTING_CONTRACT_ADDR].storage[padIndex(4,true)] = utils.intToHex(input.blockMakers.length);
+  mapAddresses(5,input.blockMakers, VOTING_CONTRACT_ADDR);
+}
+
+function buildGovernanceStorage(input){
+  mapAddresses(0, input.governanceOwners, GOVERNANCE_CONTRACT_ADDR);
+  template['alloc'][GOVERNANCE_CONTRACT_ADDR].storage[padIndex(1,true)] = utils.intToHex(input.governanceOwners.length);
 }
 
 function fundAddresses(input) {
-  let all = input.voters.concat(input.makers);
+  let all = uniq(input.blockMakers
+    .concat(input.blockVoters)
+    .concat(input.governanceOwners));
   for(let i=0; i<all.length; i++) {
     template['alloc'][utils.addHexPrefix(all[i])] = { balance: "1000000000000000000000000000"};
   }
   template['alloc'][VOTING_CONTRACT_ADDR].balance = "1000000000000000000000000000";
+  template['alloc'][GOVERNANCE_CONTRACT_ADDR].balance = "1000000000000000000000000000";
 }
 
 function loadConfig() {
@@ -69,13 +79,18 @@ function loadConfig() {
     process.exit(1);
   }
 
-  if(!json.voters || json.voters.length < json.threshold) {
+  if(!json.blockVoters || json.blockVoters.length < json.threshold) {
     console.log(" > Voter addresses missing or less than the threshold" );
     process.exit(1);
   }
 
-  if(!json.makers || json.makers.length < 1) {
+  if(!json.blockMakers || json.blockMakers.length < 1) {
     console.log(" > BlockMaker addresses missing or less than 1" );
+    process.exit(1);
+  }
+
+  if (!json.governanceOwners || json.governanceOwners.length <1 ) {
+    console.log(" > GovernanceOwner addresses missing or less than 1");
     process.exit(1);
   }
 
@@ -84,7 +99,8 @@ function loadConfig() {
 
 function main() {
   let input = loadConfig();
-  buildStorage(input);
+  buildVotingStorage(input);
+  buildGovernanceStorage(input);
   fundAddresses(input)
   fs.writeFileSync(path.join(process.cwd(),OUTPUT), JSON.stringify(template, null, 2));
 }
